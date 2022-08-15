60a20e59dc25e7d6a9fa726ed4f0461b6c7f0e29
tearknow <904916790@qq.com>
2022-08-15 12:34:16 +0800

    tmp

--- user/models.py
+++ user/models.py
@@ -25,3 +25,22 @@ class User(models.Model):
         birth_date = datetime.date(self.birth_year, self.birth_month, self.birth_day)
         return (now - birth_date).days // 365
 
+class Profile(models.Model):
+    SEX = (
+        ('M', 'nan'),
+        ('F', 'nv')
+    )
+
+    location = models.CharField(max_length = 32, verbose_name='目标城市')
+
+    min_distance = models.IntegeField(default=1, verbose_name='最小查找范围')
+    max_distance = models.IntegeField(default=10, verbose_name='最大查找范围')
+
+    min_dating_age = models.IntegeField(default=18, verbose_name='最小交友年龄')
+    max_dating_age = models.IntegeField(default=18, verbose_name='最大交友年龄')
+
+    dating_sex = models.CharField(verbose_name='匹配的性别')
+    vibration = models.Field(verbose_name='开启震动')
+    only_matche = models.Field(verbose_name='不让为匹配的人看我的相册')
+    auto_play = models.Field(verbose_name='自动播放视频')
+
